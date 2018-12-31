0553d9dcb7c10cfc4ab561b5fe176e6671fb3d7c
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-31 20:25:16 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -9,8 +9,12 @@ require 'pry'
 # }
 
 
+def get_the_min(groceries)
+  #code your solution here!
+  
+end
 
 #def get_the_min(groceries)
   #code your solution here!
-  groceries.values.flatten.min
-end
+ # groceries.values.flatten.min
+#end
