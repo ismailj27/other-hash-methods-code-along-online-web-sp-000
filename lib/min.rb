a9f1409691439d3267f55302f72743f747bab6d3
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-31 20:29:05 +0000

    Automatically backed up by Learn

--- lib/min.rb
+++ lib/min.rb
@@ -15,7 +15,7 @@ require 'pry'
   sorted_values_list[0]
 #end
 
-#def get_the_min(groceries)
+def get_the_min(groceries)
   #code your solution here!
- # groceries.values.flatten.min
-#end
+  groceries.values.flatten.min
+end
